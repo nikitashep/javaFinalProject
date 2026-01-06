9d17c26a0c82690d4ba20eabdac864d5bb3e3fb7
nikitashep <nikitashep2007@gmail.com>
2026-01-06 20:26:41 +0100

    task1 files

---
+++ src/main/java/pl/edu/vistula/task1_java_spring/Task1JavaSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.task1_java_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task1JavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task1JavaSpringApplication.class, args);
+	}
+
+}
