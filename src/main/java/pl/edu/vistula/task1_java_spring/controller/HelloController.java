6835ca91f30ae0a44000808cd5fd2f8200d8d0b6
nikitashep <75265927+nikitashep@users.noreply.github.com>
2026-01-08 01:27:53 +0100

    task1 files

--- src/main/java/pl/edu/vistula/task1_java_spring/controller/HelloController.java
+++ src/main/java/pl/edu/vistula/task1_java_spring/controller/HelloController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping(value = "/")
+    @ResponseBody
     public String hello() {
         return "Hello Vistula, in my first Spring controller.";
     }
